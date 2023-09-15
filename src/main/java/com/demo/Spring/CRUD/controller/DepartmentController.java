657d027ecee7c0efd203dc8eeef2e63f552494ee
4usaidivya <4u.saidivya@gmail.com>
2023-09-14 21:11:16 -0500

    first commit

--- src/main/java/com/demo/Spring/CRUD/controller/DepartmentController.java
+++ src/main/java/com/demo/Spring/CRUD/controller/DepartmentController.java
@@ -1,4 +1,65 @@
 package com.demo.Spring.CRUD.controller;
 
+// Java Program to Illustrate DepartmentController.java File
+
+
+
+import java.util.List;
+// Importing required classes
+import javax.validation.Valid;
+import com.demo.Spring.CRUD.entity.Department;
+import com.demo.Spring.CRUD.service.DepartmentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+// Annotation
+@RestController
+// Class
 public class DepartmentController {
+
+    @Autowired
+    private DepartmentService departmentService;
+    Logger logger = LoggerFactory.getLogger(DepartmentController.class);
+
+    // Save operation
+    @PostMapping("/departments")
+
+    public Department saveDepartment(
+            @Valid @RequestBody Department department)
+    {
+        logger.info ("entering the controller");
+        return departmentService.saveDepartment(department);
+    }
+
+    // Read operation
+    @GetMapping("/departments")
+
+    public List<Department> fetchDepartmentList()
+    {
+        return departmentService.fetchDepartmentList();
+    }
+
+    // Update operation
+    @PutMapping("/departments/{id}")
+
+    public Department
+    updateDepartment(@RequestBody Department department,
+                     @PathVariable("id") Long departmentId)
+    {
+        return departmentService.updateDepartment(
+                department, departmentId);
+    }
+
+    // Delete operation
+    @DeleteMapping("/departments/{id}")
+
+    public String deleteDepartmentById(@PathVariable("id")
+                                       Long departmentId)
+    {
+        departmentService.deleteDepartmentById(
+                departmentId);
+        return "Deleted Successfully";
+    }
 }
--- src/main/java/com/demo/Spring/CRUD/service/DepartmentService.java
+++ src/main/java/com/demo/Spring/CRUD/service/DepartmentService.java
@@ -1,4 +1,23 @@
 package com.demo.Spring.CRUD.service;
 
+// Importing required classes
+import com.demo.Spring.CRUD.entity.Department;
+
+import java.util.List;
+
+// Class
 public interface DepartmentService {
+
+    // Save operation
+    Department saveDepartment(Department department);
+
+    // Read operation
+    List<Department> fetchDepartmentList();
+
+    // Update operation
+    Department updateDepartment(Department department,
+                                Long departmentId);
+
+    // Delete operation
+    void deleteDepartmentById(Long departmentId);
 }
